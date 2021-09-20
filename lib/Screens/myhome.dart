530e1b3732bc4df64fa9f5a55290900c8abd8885
tahira-khan <72301089+tahira-khan@users.noreply.github.com>
2021-09-20 07:01:51 +0500

    Update myhome.dart
    api

--- lib/Screens/myhome.dart
+++ lib/Screens/myhome.dart
@@ -1,6 +1,9 @@
+import 'package:fingerprint_auth_example/Screens/dashboard.dart';
+import 'package:fingerprint_auth_example/Widgets/customTextField.dart';
 import 'package:flutter/material.dart';
-import 'package:humteamaptech/Widgets/customTextField.dart';
-import 'package:humteamaptech/Screens/dashboard.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MyHome extends StatefulWidget {
   @override
@@ -8,12 +11,42 @@ class MyHome extends StatefulWidget {
 }
 
 class _MyHomeState extends State<MyHome> {
+  double width = 0, height = 0;
   TextEditingController id = TextEditingController();
   TextEditingController pass = TextEditingController();
+
   final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+
+  loginpage() async {
+    SharedPreferences _sharedPreferences =
+        await SharedPreferences.getInstance();
+    var res = await http.get(Uri.parse(
+        'https://guessitquiz.000webhostapp.com/userlogin.php?uname=' +
+            id.text +
+            '&pass=' +
+            pass.text));
+
+    var result = res.body;
+    var data = json.decode(res.body);
+    print(result);
+    if (result != null) {
+      String uname = data[0]['fullname'];
+      _sharedPreferences.setString('fullname', uname);
+      print(uname);
+      Route route = MaterialPageRoute(
+        builder: (_) => MyDashBoard(),
+      );
+      Navigator.push(context, route);
+      // _shared.setString("uname", uname);
+    } else if (result == "0") {
+      print(result);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    width = MediaQuery.of(context).size.width;
+    height = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -38,64 +71,66 @@ class _MyHomeState extends State<MyHome> {
         ),
         centerTitle: true,
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          color: Color(0xFFD5DBDF),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image(
-                  height: 150.0,
-                  width: 300.0,
-                  image: AssetImage('images/aa.png')),
-              SizedBox(
-                height: 10.0,
-              ),
-              Form(
-                key: _formkey,
-                child: Column(
-                  children: <Widget>[
-                    CustomTextField(
-                      controller: id,
-                      data: Icons.person,
-                      isObsecure: false,
-                      hintText: 'Enter Username',
-                    ),
-                    CustomTextField(
-                      controller: pass,
-                      data: Icons.lock,
-                      isObsecure: true,
-                      hintText: 'Enter Password',
-                    ),
-                  ],
+      body: SingleChildScrollView(
+        child: Container(
+          height: height,
+          decoration: BoxDecoration(
+            color: Color(0xFFD5DBDF),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image(
+                    height: 150.0,
+                    width: 300.0,
+                    image: AssetImage('images/aa.png')),
+                SizedBox(
+                  height: 10.0,
                 ),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Container(
-                height: 50.0,
-                width: 250.0,
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                child: MaterialButton(
-                  onPressed: () {
-                    Route route = MaterialPageRoute(
-                      builder: (_) => MyDashBoard(),
-                    );
-                    Navigator.push(context, route);
-                  },
-                  child: Text(
-                    'login',
-                    style: TextStyle(color: Colors.white, fontSize: 25.0),
+                Form(
+                  key: _formkey,
+                  child: Column(
+                    children: <Widget>[
+                      CustomTextField(
+                        controller: id,
+                        data: Icons.person,
+                        isObsecure: false,
+                        hintText: 'Enter Username',
+                      ),
+                      CustomTextField(
+                        controller: pass,
+                        data: Icons.lock,
+                        isObsecure: true,
+                        hintText: 'Enter Password',
+                      ),
+                    ],
                   ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 10.0,
+                ),
+                Container(
+                  height: 50.0,
+                  width: 200.0,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: MaterialButton(
+                    onPressed: () {
+                      setState(() {
+                        loginpage();
+                      });
+                    },
+                    child: Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white, fontSize: 25.0),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
